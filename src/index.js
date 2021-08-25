62876a765c9e7ee4e8a0bc9d15a206924d01e96f
Garrett Griffin <griffin.garrett@gmail.com>
2021-08-25 15:57:49 -0500

    chore: updated router basename (last time..?)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/React">
+    <Router basename="/">
       <App />
     </Router>
   </React.StrictMode>,
