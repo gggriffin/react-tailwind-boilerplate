a62993f09fdd9b78cb87783e877decec08580da3
Garrett Griffin <griffin.garrett@gmail.com>
2021-08-24 23:28:44 -0500

    chore: updated BrowserRouter's basename

--- src/App.js
+++ src/App.js
@@ -9,17 +9,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      Test test test
-      <Router basename="https://github.com/gggriffin/deadwoods">
-        <FullWidthContainer>
-          <Header />
-          <Content />
-          <NavBar />
-          <Footer />
-        </FullWidthContainer>
-      </Router>
-    </>
+    <Router basename="/React">
+      <FullWidthContainer>
+        <Header />
+        <Content />
+        <NavBar />
+        <Footer />
+      </FullWidthContainer>
+    </Router>
   );
 }
 
