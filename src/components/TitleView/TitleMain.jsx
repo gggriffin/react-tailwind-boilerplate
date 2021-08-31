0066fdcebca622283e8660aedfe5a431acefe668
Garrett Griffin <griffin.garrett@gmail.com>
2021-08-30 23:19:45 -0500

    fix: update title to faq link

--- src/components/TitleView/TitleMain.jsx
+++ src/components/TitleView/TitleMain.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const TitleMain = () => {
   return (
     <div className='font-main-content-text lg:mt-60 text-white text-4xl text-center'>
-      <a href="/faq">
+      <a href="deadwoods.org/faq">
         <h1>MORE INFO HERE</h1>
       </a>
     </div>
