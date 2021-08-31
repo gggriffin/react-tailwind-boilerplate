ffa20c4d723ff954096a20d42a27b067e2951cbf
Garrett Griffin <griffin.garrett@gmail.com>
2021-08-30 23:23:35 -0500

    fix: changed href to Link

--- src/components/TitleView/TitleMain.jsx
+++ src/components/TitleView/TitleMain.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const TitleMain = () => {
   return (
     <div className='font-main-content-text lg:mt-60 text-white text-4xl text-center'>
-      <a href="deadwoods.org/faq">
+      <Link to='/faq'>
         <h1>MORE INFO HERE</h1>
-      </a>
+      </Link>
     </div>
   );
 };
