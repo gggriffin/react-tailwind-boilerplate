2769f9e9f4e6aac39be631447c11debf9448bcb8
Garrett Griffin <griffin.garrett@gmail.com>
2021-09-02 00:08:48 -0500

    feat: second title view implemented

--- src/components/TitleView/TitleIntro.jsx
+++ src/components/TitleView/TitleIntro.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const TitleIntro = () => {
   return (
-    <div className='flex text-green-primary justify-center'>
+    <div className='flex text-green-primary justify-center mt-8'>
       <div className='font-main-content-text text-5xl lg:text-7xl lg:mt-32 text-center'>
-        <h2 className='mb-6 lg:mb-12'>10/30/21</h2>
+        <h2 className='mb-3 lg:mb-12'>10/30/21</h2>
         <h2>LaCygne, KS</h2>
       </div>
     </div>
--- src/components/TitleView/TitleView.jsx
+++ src/components/TitleView/TitleView.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { TitleIntro } from "./TitleIntro";
-import { TitleMain } from "./TitleMain";
+import { InfoTile } from "../Wrappers/InfoTile";
 export const TitleView = () => {
   return (
-    <div className="h-full flex flex-col gap-y-20 mt-10">
+    <>
       <TitleIntro />
-      <TitleMain />
-    </div>
+      <InfoTile>
+        Welcome to The Deadwoods, where scares come easy and thrills are many.
+        The creatures of the night beckon you to take a scream filled tour
+        through our haunted corn trail, where you will come face-to-face with
+        your worst fears and nightmares. If you have the nerve after, climb on
+        board our hayride of terror as it transports you through a dark forest
+        filled with creeps, specters, and killers sure to make your insides
+        gurgle. We wish you the best of luck clinging to what remains of your
+        sanity after your journey through this Hellscape. Come see us, we dare
+        you... ˝
+      </InfoTile>
+    </>
   );
 };
